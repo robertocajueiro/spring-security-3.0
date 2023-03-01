b646e61c39629d0ec86e0dea29ecc569bd355107
robertocajueiro <beto.cajueiro@gmail.com>
2023-03-01 13:26:08 -0300

    applied security and testing

--- src/main/java/com/roberto/apiwithsecurity/controller/ProductController.java
+++ src/main/java/com/roberto/apiwithsecurity/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.roberto.apiwithsecurity.dto.Product;
 import com.roberto.apiwithsecurity.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class ProductController {
     @GetMapping("/all")
     public List<Product> getAllTheProducts(){
         return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
     }
 }
